01c374eb5486ba68b4dbe1b5ff4c16018a1c7450
Bright Nkrumah <brytey2k@gmail.com>
2023-02-03 14:13:47 +0000

    Feature: change homepage to login screen

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()
+        ->route('login');
 });
 
 Route::get('/dashboard', function () {
