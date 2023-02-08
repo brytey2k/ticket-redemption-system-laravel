2f7c53ab4d6339647a109634dca61e14ad33e3b2
Bright Nkrumah <brytey2k@gmail.com>
2023-02-08 12:18:05 +0000

    Chore: slack logging disabled to prevent errors for unconfigured setups
    This has only been commented out in the logging config file. When the
    setup is done and the webhook url is ready, it can be put in the .env
    file and re-enabled in the logging.php config file

--- config/logging.php
+++ config/logging.php
@@ -53,7 +53,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single', 'slack'],
+            'channels' => ['single'/*, 'slack'*/],
             'ignore_exceptions' => false,
         ],
 
